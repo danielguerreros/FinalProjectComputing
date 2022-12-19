52c156b2e9e244a3bfd7c86bbe4beeb5ac77f780
sim9771 <shim9771@gmail.com>
2022-12-19 14:08:39 -0500

    added documentation

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#'
+#' @docType data
+#'
+#' @usage data(epilepsy)
+#'
+#' @format An object of class `"cross"`; see [qtl::read.cross()].
+#'
+#' @keywords datasets
+#'
+#' @source epilepsy data
+#'
+#' @examples
+#' data(epilepsy)
+"epilepsy"
